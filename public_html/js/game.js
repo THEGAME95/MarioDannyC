d70e278ead9e207aafdfa875d194fa8f8534bf3f
Daniel Cruz <cruz41079@gmail.com>
2014-12-16 11:48:40 -0800

    c

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -41,10 +41,10 @@ var game = {
 	"loaded" : function () {
                 me.pool.register("mario", game.PlayerEntity, true);
                 me.pool.register("BadGuy", game.BadGuy);
-                me.pool.register("mushroom", game.mushroom)
+                me.pool.register("Mushroom", game.Mushroom)
             
                 me.pool.register("levelTrigger", game.LevelTrigger);
-                me.pool.register("levelTrigger", game.LevelTrigger);
+                //me.pool.register("levelTrigger", game.LevelTrigger);
             
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
